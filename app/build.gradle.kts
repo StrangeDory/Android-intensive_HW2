841a5b6ab746ceceefbee783f7422a67af024ebe
katerina3752 <ekaterinaborodich043@gmail.com>
2023-11-30 16:54:53 +0300

    picsDone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -44,4 +47,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //CoroutineScope
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
